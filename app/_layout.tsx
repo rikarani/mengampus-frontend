f25a2d045ee84d2846379daa625fa8adf891b74a
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-04 10:59:46 +0700

    done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,21 @@
 import "../global.css";
+import { useState } from "react";
 
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)/login" />
         <Stack.Screen name="(auth)/register" />
+
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name="(events)" />
+        </Stack.Protected>
       </Stack>
     </GestureHandlerRootView>
   );
