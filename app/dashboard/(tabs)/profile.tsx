afe583f66c05ac2728ee6826d520fb5bc3a4b0ee
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-05 15:29:20 +0700

    feat: add ProfilePage component for user profile display

---
+++ app/dashboard/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import { StyledSafeAreaView } from "@/components/styled/StyledSafeAreaView";
+import { Text } from "react-native";
+
+export default function ProfilePage() {
+  return (
+    <StyledSafeAreaView>
+      <Text>Profile Page</Text>
+    </StyledSafeAreaView>
+  );
+}
