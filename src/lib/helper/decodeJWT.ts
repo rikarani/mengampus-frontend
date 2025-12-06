96568651b1068fbf115fcbb2be65a2b78c5398e8
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-06 11:25:04 +0700

    delete file

--- src/lib/helper/decodeJWT.ts
+++
@@ -1,9 +0,0 @@
-export const decodeJWT = (token: string): Record<string, any> | null => {
-  try {
-    const payload = token.split(".")[1];
-
-    return JSON.parse(atob(payload));
-  } catch {
-    return null;
-  }
-};
