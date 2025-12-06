d9400e562f595648fae40c75ed0ae7e336e7e576
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 09:18:35 +0700

    Create my-event.tsx

---
+++ app/event/(tabs)/my-event.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+import { StyledSafeAreaView } from "@/components/styled/StyledSafeAreaView";
+
+export default function MyEventScreen(): React.JSX.Element {
+  return (
+    <StyledSafeAreaView>
+      <Text>My Event Screen</Text>
+    </StyledSafeAreaView>
+  );
+}
