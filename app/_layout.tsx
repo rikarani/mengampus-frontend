2febc53c59f12fea3d38bc184bac86ee901c652a
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 09:18:25 +0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,40 @@
 import "../global.css";
 
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context";
 
-import { App } from "@/components/App";
-import { AuthProvider } from "@/context/AuthContext";
+import { router, Slot } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+import { HeroUINativeProvider } from "heroui-native";
+
+import { auth } from "@/lib/auth";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const { data: session, isPending } = auth.useSession();
+
+  useEffect(() => {
+    if (!isPending) {
+      SplashScreen.hideAsync();
+
+      if (!session) {
+        router.replace("/(auth)/login");
+      } else {
+        router.replace("/dashboard/profile");
+      }
+    }
+  }, [isPending, session]);
+
   return (
-    <AuthProvider>
-      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <App />
-        </GestureHandlerRootView>
-      </SafeAreaProvider>
-    </AuthProvider>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <HeroUINativeProvider>
+          <Slot />
+        </HeroUINativeProvider>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
