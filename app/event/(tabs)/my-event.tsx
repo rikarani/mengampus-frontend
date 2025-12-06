65f68f25cb8f383d2ce2b9b61a24a123102d9a97
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-06 17:52:00 +0700

    Refactor MyEventScreen: implement event fetching and display with loading state

--- app/event/(tabs)/my-event.tsx
+++ app/event/(tabs)/my-event.tsx
@@ -1,11 +1,76 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
 
-import { StyledSafeAreaView } from "@/components/styled/StyledSafeAreaView";
+import { FlatList, Text, View } from "react-native";
+
+import { Button, Card } from "heroui-native";
+
+import { API_HOST } from "@/constants";
+
+type Event = {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  location: string;
+  date: Date;
+  created_at: Date;
+  updated_at: Date;
+};
+
+const formatter = new Intl.DateTimeFormat("id-ID", {
+  day: "2-digit",
+  month: "long",
+  year: "numeric",
+});
 
 export default function MyEventScreen(): React.JSX.Element {
-  return (
-    <StyledSafeAreaView>
-      <Text>My Event Screen</Text>
-    </StyledSafeAreaView>
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [events, setEvents] = useState<Event[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${API_HOST}/events`);
+      const data = await response.json();
+
+      if (data.success) {
+        setEvents(data.data);
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  return isLoading ? (
+    <View>
+      <Text>Loading...</Text>
+    </View>
+  ) : (
+    <FlatList
+      data={events}
+      keyExtractor={(event) => event.id}
+      renderItem={({ item, index }) => (
+        <Card className={`rounded-none ${index === events.length - 1 ? undefined : "mb-4"}`}>
+          <Card.Header>
+            <Text className="text-white">
+              {formatter.format(new Date(item.date))} - {item.category}
+            </Text>
+            <Text className="text-white">{item.location}</Text>
+          </Card.Header>
+          <Card.Body>
+            <Card.Label>{item.name}</Card.Label>
+            <Card.Description>{item.description}</Card.Description>
+          </Card.Body>
+          <Card.Footer>
+            <View className="flex flex-row gap-2">
+              <Button className="flex-1">
+                <Text>Lihat Detail Event</Text>
+              </Button>
+              <Button className="flex-1">
+                <Text>Enroll Event</Text>
+              </Button>
+            </View>
+          </Card.Footer>
+        </Card>
+      )}
+    />
   );
 }
