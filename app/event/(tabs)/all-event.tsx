aabd5b8e68f47115442072be0df5a07ee0d82abf
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-06 09:18:32 +0700

    Create all-event.tsx

---
+++ app/event/(tabs)/all-event.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+import { StyledSafeAreaView } from "@/components/styled/StyledSafeAreaView";
+
+export default function AllEventScreen(): React.JSX.Element {
+  return (
+    <StyledSafeAreaView>
+      <Text>All Event Screen</Text>
+    </StyledSafeAreaView>
+  );
+}
