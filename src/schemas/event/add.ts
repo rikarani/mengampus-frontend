745d8dae571c8c29771aa342bb7d039435910425
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-07 22:09:00 +0700

    Update add.ts

--- src/schemas/event/add.ts
+++ src/schemas/event/add.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 
 export const addEventSchema = z.object({
   name: z.string().min(1, { error: "Nama event wajib diisi" }),
+  category_id: z.string().min(1, { error: "Kategori event wajib diisi" }),
   date: z.iso.date({ error: "Tanggal event wajib diisi" }),
   time: z.iso.time({ error: "Waktu event wajib diisi" }),
-  category: z.string().min(1, { error: "Kategori event wajib diisi" }),
   description: z.string().min(1, { error: "Deskripsi event wajib diisi" }),
   location: z.string().min(1, { error: "Lokasi event wajib diisi" }),
 });
