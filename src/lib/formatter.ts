3782feb64c2728ec52aad06520d0f1f9d7c1fb52
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-07 22:08:58 +0700

    Update formatter.ts

--- src/lib/formatter.ts
+++ src/lib/formatter.ts
@@ -7,12 +7,3 @@ export const dateFormatter = (date: Date) => {
 
   return formatter.format(date);
 };
-
-export const timeFormatter = (date: Date) => {
-  const formatter = new Intl.DateTimeFormat("id-ID", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-
-  return formatter.format(date);
-};
