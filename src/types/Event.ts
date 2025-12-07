293ff5919e225f9c1cdc414e1e92187d60eed2e6
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2025-12-07 22:09:02 +0700

    Update Event.ts

--- src/types/Event.ts
+++ src/types/Event.ts
@@ -4,7 +4,8 @@ export type Event = {
   id: string;
   category_id: string;
   name: string;
-  date: Date;
+  date: string;
+  time: string;
   description: string;
   location: string;
   created_at: Date;
