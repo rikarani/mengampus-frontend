a39cf311810d2f267078068474fa99b22674419a
rikarani <46773591+rikarani@users.noreply.github.com>
2025-12-09 16:27:36 +0700

    delete

--- src/lib/formatter.ts
+++
@@ -1,9 +0,0 @@
-export const dateFormatter = (date: Date) => {
-  const formatter = new Intl.DateTimeFormat("id-ID", {
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  });
-
-  return formatter.format(date);
-};
